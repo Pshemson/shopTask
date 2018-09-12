d70229ce08922bea242da4d0d17da347bcb93c12
Przemek Bednarczyk <pjbednarczyk@gmail.com>
2018-09-12 20:17:21 +0200

    add toggling products visibility

--- js/app.js
+++ js/app.js
@@ -2,64 +2,64 @@ const settings = {
   visibleProducts: 4
 };
 
-const renderProduct = (product, ul, isVisible) => {
+const renderProduct = (product, ul) => {
   const productContainer = document.createElement(`li`);
-productContainer.classList.add('singleProduct');
-
-if (isVisible) {
-  productContainer.classList.add('visible');
-} else {
-  productContainer.classList.remove('visible');
-}
-
-productContainer.innerHTML = `
+  productContainer.classList.add('singleProduct');
+  const saveAmount = product.price.gross.base_float - product.price.gross.promo_float;
+  productContainer.innerHTML = `
 <div class="cartBox">
             <img src="img/cart_icon.png">
             <span>sztuk: ${product.stockId}</span>
           </div>
           <div class="saveAmount">
             <p>oszczędzasz:
-              <span class="savePrice">2000,00 zł </span>
+              <span class="savePrice">${saveAmount} zł </span>
             </p>
             <div class="prodImage">
               <a href="#"><img src="https://outletmeblowy.pl/environment/cache/images/300_300_productGfx_${product.main_image}.jpg"></a>
             </div>
             <div class="productInfo">
 <span class="priceNew">${product.price.gross.final}</span>
-              <span class="priceOld">5690 zł</span>
+              <span class="priceOld">${product.price.gross.base}</span>
               <a href="#"><h2>${product.name}</h2></a> 
               <span class="nameBrand"><a href="#">${product.producer.name}</a></span>
             </div>
           </div>`;
 
-ul.appendChild(productContainer)
-
+  return ul.appendChild(productContainer)
 };
 
+const toggleProductsVisibility = () => {
+  const [...allProducts] = document.querySelectorAll('#products li');
+  allProducts.map((product, index) => {
 
+      if (index+1 <= settings.visibleProducts) {
+        return product.classList.add('visible');
+      }
 
-document.addEventListener("DOMContentLoaded", function(event) {
+      return product.classList.remove('visible');
+  })
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
   console.log(data);
 
-const productList = document.querySelector('#products');
-const buttons = [...document.querySelectorAll('#productsMenu b')];
+  const productList = document.querySelector('#products');
 
-buttons.map(button => button.addEventListener("click", () => {
-  settings.visibleProducts = Number(button.innerText);
-  return renderProducts();
-}));
+ // pseudo-tablica z elementami <b></b>
+  const buttons = document.querySelectorAll('#productsMenu b');
 
-const renderProducts = () => {
-  return data.list.map((item, i) => {
-    if (i+1 <= settings.visibleProducts) {
-      return renderProduct(item, productList, true)
-    }
+  // zamieniam na zwykla tablice
+  const buttonsArray = [...buttons];
 
-    return renderProduct(item, productList, false)
-  });
-};
+  buttonsArray.map(button => button.addEventListener("click", () => {
+    settings.visibleProducts = Number(button.innerText);
+    return toggleProductsVisibility();
+  }));
 
-  renderProducts()
+  const renderProducts = () => data.list.map(item => renderProduct(item, productList));
 
+  renderProducts();
+  return toggleProductsVisibility();
 
 });
